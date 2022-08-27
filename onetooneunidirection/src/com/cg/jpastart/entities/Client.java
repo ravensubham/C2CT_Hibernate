024d7dfcf2b26ca6b201740b5b32dd9b83530222
shubham raj <71871642+ravensubham@users.noreply.github.com>
2022-08-27 13:33:52 +0530

    Add files via upload
    first commit

---
+++ onetooneunidirection/src/com/cg/jpastart/entities/Client.java
@@ -0,0 +1,44 @@
+package com.cg.jpastart.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Client {
+
+	
+	public static void main(String[] args) {
+	
+	
+	EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU"); 
+	EntityManager em = factory.createEntityManager();
+    
+	em.getTransaction().begin();
+
+	Student student = new Student();
+
+	student.setName("subham");
+
+	
+	Address homeAddress = new Address(); 
+	homeAddress.setStreet ("karkarbagh Road");
+
+	homeAddress.setCity("patna");
+
+	homeAddress.setState("Bihar"); 
+	homeAddress.setZipCode("800 026");
+
+	// Inject address to student table ?? student.setAddress(homeAddress);
+
+	// persist
+
+	em.persist(student);
+
+	em.getTransaction().commit();
+
+	em.close(); 
+	factory.close();
+	
+	}
+	
+}
